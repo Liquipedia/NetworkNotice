1b4e511f2f1977286f8a725761ca9c014a0f3a64
Laura van Helvoort <lvanhelvoort@teamliquid.com>
2022-06-29 12:43:29 +0200

    remove selectable options and revert remove of inverse class

--- src/NoticeHtml.php
+++ src/NoticeHtml.php
@@ -9,14 +9,15 @@ class NoticeHtml {
 	/**
 	 * Generate the Html for out notice
 	 * @param OutputPage $outputPage
+	 * @param string $style
 	 * @param string $text
 	 * @param string $id
 	 * @return string Html for our notice
 	 */
-	public static function getNoticeHTML( $outputPage, $text, $id = '0' ) {
+	public static function getNoticeHTML( $outputPage, $style, $text, $id = '0' ) {
 		$classes = [
 			'networknotice',
-			'networknotice-default',
+			'networknotice-' . $style,
 		];
 		$attributes = [
 			'id' => 'networknotice-' . $id,
